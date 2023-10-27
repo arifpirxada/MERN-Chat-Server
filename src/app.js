f741cf70a6f1887d41eba2d7fc8c3d12fc4dfe16
Arif <arifpirxada@gmail.com>
2023-10-27 16:47:13 +0530

    update - changed cors origin to real domain

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const server = createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://verbula.netlify.app",
   },
 });
 
